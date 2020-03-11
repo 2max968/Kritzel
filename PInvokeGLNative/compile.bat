82b45db16a389075195fbcb4067bc036108b4d88
Max <maximilian-haag@web.de>
2020-03-12 00:45:51 +0100

    Try 2

--- PInvokeGLNative/compile.bat
+++ PInvokeGLNative/compile.bat
@@ -1,2 +1,2 @@
 g++ -DGLEW_STATIC nativegl.cpp -o nativegl.dll -shared -Wall -O2 -I./include -L./lib -lglfw3 -lglew32 -lopengl32 -lkernel32 -luser32 -lgdi32
-copy nativegl.dll ..\nativegl.dll /Y
+copy nativegl.dll ..\PInvokeGL\nativegl.dll /Y
---
+++ PointerInputLibrary/PointerInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointerInputLibrary
+{
+    public enum PenFlags
+    {
+        NONE = 0,
+        BARREL = 1,
+        INVERTED = 2,
+        ERASER = 4
+    }
+
+    public class PointerInfo
+    { 
+        [DllImport("StylusLib.dll")]
+        public static extern void SomeFunction();
+
+        [DllImport("user32.dll")]
+        public static extern bool GetPointerPenInfo(UInt32 id, out POINTER_PEN_INFO info);
+        
+        [StructLayout(LayoutKind.Explicit)]
+        public struct POINTER_PEN_INFO
+        {
+            [FieldOffset(96)]  public uint x86pressure;
+            [FieldOffset(104)] public uint x64pressure;
+            [FieldOffset(88)] public PenFlags x86penFlags;
+            [FieldOffset(96)] public PenFlags x64penFlags;
+            [FieldOffset(200)]
+            private int nil;
+        }
+    }
+}
